9e71893d6bfd905f67a87f31d66e73794049b104
roicenir <roicenir2@hotmail.com>
2019-11-19 23:22:19 -0300

    a

--- bundle.js
+++ bundle.js
@@ -2042,27 +2042,29 @@ exports.write = function (buffer, value, offset, isLE, mLen, nBytes) {
 }
 
 },{}],4:[function(require,module,exports){
-const magnet = require('magnet-uri')
+function obtemURI() {
+  const magnet = require("magnet-uri");
 
-const uri = magnet.encode({
-  xt: [
-    'urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1',
-    'urn:tree:tiger:7N5OAMRNGMSSEUE3ORHOKWN4WWIQ5X4EBOOTLJY',
-    'urn:btih:QHQXPYWMACKDWKP47RRVIV7VOURXFE5Q'
-  ],
-  xl: '10826029',
-  dn: 'mediawiki-1.15.1.tar.gz',
-  tr: [
-    'udp://tracker.openbittorrent.com:80/announce'
-  ],
-  as: 'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz',
-  xs: [
-    'http://cache.example.org/XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5',
-    'dchub://example.org'
-  ]
-})
+  const uri = magnet.encode({
+    xt: [
+      "urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1",
+      "urn:tree:tiger:7N5OAMRNGMSSEUE3ORHOKWN4WWIQ5X4EBOOTLJY",
+      "urn:btih:QHQXPYWMACKDWKP47RRVIV7VOURXFE5Q"
+    ],
+    xl: "10826029",
+    dn: "mediawiki-1.15.1.tar.gz",
+    tr: ["udp://tracker.openbittorrent.com:80/announce"],
+    as: "./abertura.mp4",
+    xs: [
+      "http://cache.example.org/XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5",
+      "dchub://example.org"
+    ]
+  });
 
-console.log(uri) // the magnet uri
+  console.log(uri); // the magnet uri
+  return uri;
+}
+
 },{"magnet-uri":5}],5:[function(require,module,exports){
 (function (Buffer){
 module.exports = magnetURIDecode
